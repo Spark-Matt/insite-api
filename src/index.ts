9059967280f3b0c410d6dc2e814d2de24a3fa899
Jordan Jones <proxima.aust@gmail.com>
2019-05-23 08:13:18 -0500

    Imported graphql schema

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import 'reflect-metadata'
-import { GraphQLServer } from 'graphql-yoga'
+import { importSchema } from 'graphql-import'
+import { ApolloServer } from 'apollo-server'
+import * as path from 'path'
 
-const typeDefs = `
+const typeDefs = importSchema(path.join(__dirname, 'schema.graphql'))
+const resolvers = {
+  Query: {},
+}
 
-`
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`)
+})
