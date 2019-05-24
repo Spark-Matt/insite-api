4e3a03e2bc64ca2cef2ad62084e1bdf0498bf8bb
Jordan Jones <proxima.aust@gmail.com>
2019-05-24 10:11:00 -0500

    Entity files added

--- src/entity/Memory.ts
+++ src/entity/Memory.ts
@@ -1,21 +1,38 @@
-import {
-  Entity,
-  PrimaryColumn,
-  Column,
-  BeforeInsert,
-  BaseEntity,
-} from 'typeorm'
-import * as uuid from 'uuid/v4'
+import { Entity, Column, BaseEntity, PrimaryGeneratedColumn } from 'typeorm'
 
-@Entity()
-export class Memory extends BaseEntity {
-  @PrimaryColumn('uuid') id: string
+@Entity('memory')
+export default class Memory extends BaseEntity {
+  //Dummy Field to ensure primary key
+  @PrimaryGeneratedColumn()
+  datapoint_id: number
 
-  @Column()
+  @Column('uuid')
+  id: string
+
+  @Column('bigint')
   timestamp: number
 
-  @BeforeInsert()
-  addId() {
-    this.id = uuid()
-  }
+  @Column('bigint')
+  total: number
+
+  @Column('bigint')
+  free: number
+
+  @Column('bigint')
+  used: number
+
+  @Column('bigint')
+  active: number
+
+  @Column('bigint')
+  available: number
+
+  @Column('bigint')
+  buffcache: number
+
+  @Column('bigint')
+  swaptotal: number
+
+  @Column()
+  swapused: number
 }
