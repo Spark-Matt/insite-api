549f708b383803f6143a03d03606469e78707519
Jordan Jones <proxima.aust@gmail.com>
2019-05-27 12:16:34 -0500

    Consume more entities from POST (Time)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,69 @@
 import * as express from 'express'
 import { Router } from 'express'
 import Memory from '../entity/Memory'
+import Client from '../entity/Client'
+import User from '../entity/User'
+import Battery from '../entity/Battery'
+import OperatingSystem from '../entity/OperatingSystem'
+import Time from '../entity/Time'
 
 const app = Router()
 
 app.post('/post', async (req: express.Request, res: express.Response) => {
   //Future route for accepting data!
-  const data = req.body.mem
+  //If POST doesnt have a body, reject
+  if (!req.body) return res.end()
+
   //UTC Timestamp
-  data.timestamp = Math.floor(Date.now() / 1000)
+  const timestamp = Math.floor(Date.now() / 1000)
   //static id, system server feature coming soon
-  data.id = '9624af56-4f5f-4da2-89e0-7a41f2642061'
-  const memory = Memory.create({ ...data })
-  //Save to database
-  await memory.save()
+  const id = '9624af56-4f5f-4da2-89e0-7a41f2642061'
+
+  //const { _id: id } = req.body
+  const client = Client.create({
+    id,
+    lastTimestamp: timestamp,
+  })
+  await client.save()
+
+  //Save CPU
+  const { os } = req.body
+  await OperatingSystem.create({
+    id,
+    timestamp,
+    ...os,
+  }).save()
+
+  //Save timezone
+  const { time } = req.body
+  await Time.create({
+    id,
+    timestamp,
+    ...time,
+  }).save()
+
+  //Save memory
+  const memory = req.body.mem
+  await Memory.create({ ...memory, timestamp, id })
+
+  //Get users
+  const users = req.body.users
+  users.map(
+    async (user: User) =>
+      await User.create({
+        ...user,
+        id,
+        timestamp,
+      }).save()
+  )
+  //Get Battery data
+  const battery = req.body.battery
+  await Battery.create({
+    id,
+    timestamp,
+    ...battery,
+  }).save()
+
   res.end()
 })
 
