dca8f5e837efa026ab2dde7e26f176c9e148a43e
Jordan Jones <proxima.aust@gmail.com>
2019-05-27 12:14:58 -0500

    Use apollo-server makeExecutableSchema instead

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 import 'reflect-metadata'
 import { importSchema } from 'graphql-import'
-import { ApolloServer } from 'apollo-server-express'
+import { ApolloServer, makeExecutableSchema } from 'apollo-server-express'
 import { createConnection } from 'typeorm'
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
 import * as path from 'path'
 
 import Routes from './routes'
+import resolvers from './resolvers'
 
-const typeDefs = importSchema(path.join(__dirname, 'schema.graphql'))
-const resolvers = {
-  Query: {},
-}
-
-const server = new ApolloServer({ typeDefs, resolvers })
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(Routes)
+
+const typeDefs = importSchema(path.join(__dirname, 'schema.graphql'))
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+const server = new ApolloServer({ schema })
+//apply Express server to Apollo
 server.applyMiddleware({ app })
 
 createConnection().then(async () => {
